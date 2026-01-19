abde638075ea7fede095dc4e80daabae02d5b729
shrikardongre <shrikardongre@gmail.com>
2026-01-19 12:28:31 +0530

    Diagnostics added

---
+++ include/tinylang/Basic/Diagnostic.h
@@ -0,0 +1,49 @@
+#ifndef TINYLANG_BASIC_DIAGNOSTIC_H
+#define TINYLANG_BASIC_DIAGNOSTIC_H
+
+#include "./LLVM.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/FormatVariadic.h"
+#include "llvm/Support/SMLoc.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
+#include <utility>
+
+namespace tinylang {
+
+// To not pollute the global namespace a nested namespace called diag is used to
+// include the .def file
+namespace diag {
+enum {
+#define DIAG(ID, Level, Msg) ID,
+#include "./Diagnostic.def"
+};
+} // namespace diag
+
+class DiagnosticsEngine {
+  static const char *getDiagnosticText(unsigned DiagID);
+  static SourceMgr::DiagKind getDiagnosticKind(unsigned DiagID);
+
+  SourceMgr &SrcMgr;
+  unsigned NumErrors;
+
+public:
+  DiagnosticsEngine(SourceMgr &SrcMgr) : SrcMgr(SrcMgr), NumErrors(0) {}
+
+  unsigned numErrors() { return NumErrors; }
+
+  template <typename... Args>
+  // This is a variadic template
+  void report(SMLoc Loc, unsigned DiagID, Args &&...Arguments) {
+    // Here the Msg can have variable parameters so a variadic template is used. 
+    std::string Msg = llvm::formatv(getDiagnosticText(DiagID),
+                                    std::forward<Args>(Arguments)...).str();
+    SourceMgr::DiagKind Kind = getDiagnosticKind(DiagID);
+    SrcMgr.PrintMessage(Loc, Kind, Msg);
+    NumErrors += (Kind == SourceMgr::DK_Error);
+  }
+};
+
+} // namespace tinylang
+
+#endif
---
+++ lib/Basic/Diagnostic.cpp
@@ -0,0 +1,24 @@
+#include "../../include/tinylang/Basic/Diagnostic.h"
+
+using namespace tinylang;
+
+namespace {
+const char *DiagnosticText[] = {
+#define DIAG(ID, Level, Msg) Msg,
+#include "../../include/tinylang/Basic/Diagnostic.def"
+};
+SourceMgr::DiagKind DiagnosticKind[] = {
+#define DIAG(ID, Level, Msg) SourceMgr::DK_##Level,
+#include "../../include/tinylang/Basic/Diagnostic.h"
+};
+} // namespace
+
+const char *
+DiagnosticsEngine::getDiagnosticText(unsigned DiagID) {
+  return DiagnosticText[DiagID];
+}
+
+SourceMgr::DiagKind
+DiagnosticsEngine::getDiagnosticKind(unsigned DiagID) {
+  return DiagnosticKind[DiagID];
+}
